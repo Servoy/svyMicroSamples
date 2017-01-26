58489f54f120274004f0c5b0b7ab98308b885935
paronne <paronne@servoy.com>
2017-01-26 15:33:12 +0100

    svyGroupingGrid

---
+++ svyGroupingGrid/forms/ordersTable.js
@@ -0,0 +1,36 @@
+/**
+ * @type {String}
+ *
+ * @properties={typeid:35,uuid:"552115E9-2223-43FD-A8E0-7D37B4EECDB8"}
+ */
+var count = null;
+
+
+/**
+ * Perform the element default action.
+ *
+ * @param {JSEvent} event the event that triggered the action
+ *
+ * @protected
+ *
+ * @properties={typeid:24,uuid:"E08CD2EC-6B7A-4274-A0FA-130D907A3E9B"}
+ */
+function onAction(event) {
+	// TODO Auto-generated method stub
+	foundset.find()
+}
+
+/**
+ * Perform the element default action.
+ *
+ * @param {JSEvent} event the event that triggered the action
+ *
+ * @protected
+ *
+ * @properties={typeid:24,uuid:"7B2EAF05-FC71-4DDC-89F8-29D5C7DB440E"}
+ * @AllowToRunInFind
+ */
+function onAction1(event) {
+	// TODO Auto-generated method stub
+	foundset.search()
+}
