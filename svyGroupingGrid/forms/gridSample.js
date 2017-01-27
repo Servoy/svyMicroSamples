ad13962406c30bbe87bec8c06d8744bce7636f22
paronne <paronne@servoy.com>
2017-01-27 16:09:54 +0100

    icon grid

--- svyGroupingGrid/forms/gridSample.js
+++ svyGroupingGrid/forms/gridSample.js
@@ -20,3 +20,14 @@ function getName() {
 function getDescription() {
 	return "Grid with grouping capabilities";
 }
+
+
+/**
+*
+* @return {String}
+*
+* @properties={typeid:24,uuid:"FA2DA90F-9214-4530-9599-561180C42AB9"}
+*/
+function getIconStyleClass() {
+	return "fa fa-list-alt"
+}
