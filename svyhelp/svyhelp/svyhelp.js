ade4a9be44c2c4f215352063d532fb5017471fa8
Patrick Ruhsert <pruhsert@servoy.com>
2017-01-27 16:21:22 +0100

    added help-demo to svyhelp

--- svyhelp/svyhelp/svyhelp.js
+++ svyhelp/svyhelp/svyhelp.js
@@ -1,68 +1,11 @@
-angular.module('svyhelp', ['servoy'])
-	.directive('svyLabel', function($window, $services, $sabloUtils) {
+angular.module('svyhelp', ['servoy']).directive('help-demo', function($services) {
 		return {
-			restrict: 'C',
+			restrict: 'A',
 			controller: function($scope, $element, $attrs) {
-				var serviceScope = $services.getServiceScope('svyhelp');
-				var bindTo = serviceScope.model.helpEvent;
-				
-				serviceScope.createJSEvent = function(event) {
-					var targetEl;
-					if (event.target) targetEl = event.target;
-					else if (event.srcElement) targetEl = event.srcElement;
-
-					var form;
-					var parent = targetEl;
-					var targetElNameChain = new Array();
-					while (parent) {
-						form = parent.getAttribute("ng-controller");
-						if (form) {
-							break;
-						}
-						if (parent.getAttribute("name")) targetElNameChain.push(parent.getAttribute("name"));
-						parent = parent.parentNode;
-					}
-
-					//create JSEvent
-					var jsEvent = { svyType: 'JSEvent' };
-
-					//get event type
-					var eventType = 'action';
-					if (event.type == 'contextmenu') eventType = 'rightClick';
-					else if (event.type == 'dblclick') eventType = 'doubleClick';
-					jsEvent.eventType = eventType;
-
-					//get modifiers
-					var modifiers = (event.altKey ? 8 : 0) | (event.shiftKey ? 1 : 0) | (event.ctrlKey ? 2 : 0) | (event.metaKey ? 4 : 0);
-					jsEvent.modifiers = modifiers;
-
-					//TODO: are these the coordinates we need? https://support.servoy.com/browse/SVY-9010
-					jsEvent.x = event.pageX;
-					jsEvent.y = event.pageY;
-
-					jsEvent.data = null;
-
-					var formScope = angular.element(parent).scope();
-					for (var i = 0; i < targetElNameChain.length; i++) {
-						if (formScope.model[targetElNameChain[i]]) {
-							jsEvent.elementName = targetElNameChain[i];
-							break;
-						}
-					}
-					return jsEvent;
-				}
-				
-				$element.bind(bindTo, function(e) {
-						if (angular.element(this).hasClass('svy-help') && serviceScope.model.helpMode === true) {
-							var jsevent = serviceScope.createJSEvent(e);
-							jsevent.eventType = bindTo;
-							$window.executeInlineScript(serviceScope.model.callback.formname, serviceScope.model.callback.script, [jsevent]);
-						}
-					});
+				console.log('found help-demo attribute on element ' + $element.id);
 			}
-		};
-	})
-	.directive('svyTextfield', function($window, $services, $sabloUtils) {
+		}
+	}).directive('svyLabel', function($window, $services, $sabloUtils) {
 		return {
 			restrict: 'C',
 			controller: function($scope, $element, $attrs) {
@@ -114,7 +57,7 @@ angular.module('svyhelp', ['servoy'])
 					}
 					return jsEvent;
 				}
-				
+
 				$element.bind(bindTo, function(e) {
 						if (angular.element(this).hasClass('svy-help') && serviceScope.model.helpMode === true) {
 							var jsevent = serviceScope.createJSEvent(e);
@@ -124,14 +67,13 @@ angular.module('svyhelp', ['servoy'])
 					});
 			}
 		};
-	})
-	.directive('svyButton', function($window, $services, $sabloUtils) {
+	}).directive('svyTextfield', function($window, $services, $sabloUtils) {
 		return {
 			restrict: 'C',
 			controller: function($scope, $element, $attrs) {
 				var serviceScope = $services.getServiceScope('svyhelp');
 				var bindTo = serviceScope.model.helpEvent;
-			
+
 				serviceScope.createJSEvent = function(event) {
 					var targetEl;
 					if (event.target) targetEl = event.target;
@@ -177,7 +119,7 @@ angular.module('svyhelp', ['servoy'])
 					}
 					return jsEvent;
 				}
-				
+
 				$element.bind(bindTo, function(e) {
 						if (angular.element(this).hasClass('svy-help') && serviceScope.model.helpMode === true) {
 							var jsevent = serviceScope.createJSEvent(e);
@@ -187,4 +129,66 @@ angular.module('svyhelp', ['servoy'])
 					});
 			}
 		};
-	})	
+	}).directive('svyButton', function($window, $services, $sabloUtils) {
+		return {
+			restrict: 'C',
+			controller: function($scope, $element, $attrs) {
+				var serviceScope = $services.getServiceScope('svyhelp');
+				var bindTo = serviceScope.model.helpEvent;
+
+				serviceScope.createJSEvent = function(event) {
+					var targetEl;
+					if (event.target) targetEl = event.target;
+					else if (event.srcElement) targetEl = event.srcElement;
+
+					var form;
+					var parent = targetEl;
+					var targetElNameChain = new Array();
+					while (parent) {
+						form = parent.getAttribute("ng-controller");
+						if (form) {
+							break;
+						}
+						if (parent.getAttribute("name")) targetElNameChain.push(parent.getAttribute("name"));
+						parent = parent.parentNode;
+					}
+
+					//create JSEvent
+					var jsEvent = { svyType: 'JSEvent' };
+
+					//get event type
+					var eventType = 'action';
+					if (event.type == 'contextmenu') eventType = 'rightClick';
+					else if (event.type == 'dblclick') eventType = 'doubleClick';
+					jsEvent.eventType = eventType;
+
+					//get modifiers
+					var modifiers = (event.altKey ? 8 : 0) | (event.shiftKey ? 1 : 0) | (event.ctrlKey ? 2 : 0) | (event.metaKey ? 4 : 0);
+					jsEvent.modifiers = modifiers;
+
+					//TODO: are these the coordinates we need? https://support.servoy.com/browse/SVY-9010
+					jsEvent.x = event.pageX;
+					jsEvent.y = event.pageY;
+
+					jsEvent.data = null;
+
+					var formScope = angular.element(parent).scope();
+					for (var i = 0; i < targetElNameChain.length; i++) {
+						if (formScope.model[targetElNameChain[i]]) {
+							jsEvent.elementName = targetElNameChain[i];
+							break;
+						}
+					}
+					return jsEvent;
+				}
+
+				$element.bind(bindTo, function(e) {
+						if (angular.element(this).hasClass('svy-help') && serviceScope.model.helpMode === true) {
+							var jsevent = serviceScope.createJSEvent(e);
+							jsevent.eventType = bindTo;
+							$window.executeInlineScript(serviceScope.model.callback.formname, serviceScope.model.callback.script, [jsevent]);
+						}
+					});
+			}
+		};
+	}) 
