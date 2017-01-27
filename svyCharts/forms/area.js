e4801fbb4428178a526163ad12ecc12b0ca65136
Anastasija <efistasija@gmail.com>
2017-01-27 09:38:13 +0100

    Initial commit chart module

---
+++ svyCharts/forms/area.js
@@ -0,0 +1,27 @@
+/**
+ * @properties={typeid:24,uuid:"7BBF2277-0D19-4033-8304-93240E4A35B6"}
+ */
+function getName(){
+	return 'Area Chart';
+}
+
+/**
+ * @properties={typeid:24,uuid:"4081EAEC-8CF2-44B6-8E4F-DB7F457E241A"}
+ */
+function getParent(){
+	return forms.parent;
+}
+
+/**
+ * @properties={typeid:24,uuid:"09BFF289-85A6-4D7C-81DC-5CD32D364455"}
+ */
+function getDescription(){
+	return 'Area chart description'
+}
+
+/**
+ * @properties={typeid:24,uuid:"A7D30E73-4512-4897-9544-A8D6C0E061B0"}
+ */
+function getIconStyleClass() {
+	return 'fa-area-chart'
+}
