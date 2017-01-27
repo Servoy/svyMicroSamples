20484b7cf618a6ac89428191e8a8f1ca0561df78
Enrico Arata <earata@Enricos-MacBook-Air.local>
2017-01-27 14:40:09 +0100

    Added valuelist examples

---
+++ demoValuelist/forms/hardcodedValuelist.js
@@ -0,0 +1,34 @@
+/**
+ * @type {Number}
+ *
+ * @properties={typeid:35,uuid:"0ACE684D-8B09-422E-9096-75FA85116E36",variableType:8}
+ */
+var xValue = null;
+
+/**
+ * @properties={typeid:24,uuid:"FAC710FD-1DCB-4465-AFBE-756F270F2953"}
+ */
+function getName() {
+	return "Hardcoded valuelist"
+}
+
+/**
+ * @properties={typeid:24,uuid:"4C728EDB-420B-4BEB-9003-7CB612F11C06"}
+ */
+function getDescription() {
+	return "Simple Hardcoded Valuelist";
+}
+
+/**
+ * @properties={typeid:24,uuid:"43B20568-5B84-4C57-9F32-516369406BF3"}
+ */
+function getParent() {
+	return forms.valuelistparent;
+}
+
+/**
+ * @properties={typeid:24,uuid:"B398F9DA-10BD-49C9-9243-37D199B02831"}
+ */
+function getIconStyleClass() {
+	return 'fa-gear';
+}
