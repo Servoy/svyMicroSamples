bc3226eecfc4a0d8d43983cc5cfa8e291a39fe03
Sanneke <saleman@servoy.com>
2017-01-27 14:07:17 +0100

    progress

--- svyFlexFields/forms/customers.js
+++ svyFlexFields/forms/customers.js
@@ -8,3 +8,19 @@ function testMethod(event) {
 	scopes.svyFlexFields.addFieldsToForm(event,1)
 
 }
+
+/**
+ * @param oldValue
+ * @param newValue
+ * @param {JSEvent} event
+ *
+ * @return {boolean}
+ *
+ * @properties={typeid:24,uuid:"365880BE-BDD0-4FAF-A4BB-9E4B262ADA96"}
+ */
+function onDataChangeFieldSets(oldValue, newValue, event) {
+	var form = scopes.svyFlexFields.addFieldsToForm(event,foundset.svy_fieldset_id)
+	elements.tab.containedForm = form
+	
+	return true
+}
