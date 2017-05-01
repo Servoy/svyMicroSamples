2e9328c915239eb454018cb310c610497f2cf082
Dan <dlazarescu@servoy.nl>
2017-05-01 11:14:02 +0200

    no message

---
+++ exampleBasicComponents/forms/accordionPanel.js
@@ -0,0 +1,27 @@
+/**
+ * @properties={typeid:24,uuid:"27446438-C6C4-45AB-A2E6-3E66EDF285D6"}
+ */
+function getParent() {
+	return forms.basicComponents;
+}
+
+/**
+ * @properties={typeid:24,uuid:"81C4B6B1-99A0-4896-BE57-62796F91AC92"}
+ */
+function getName() {
+	return 'Accordion Panel'
+}
+
+/**
+ * @properties={typeid:24,uuid:"28223DAB-4CF1-455E-A549-594634F1AA2E"}
+ */
+function getDescription() {
+	return 'Accordion Panel Example';
+}
+
+/**
+ * @properties={typeid:24,uuid:"C431B067-CC00-4196-8C49-52529CCB134B"}
+ */
+function getIconStyleClass() {
+	return 'fa fa-th-list';
+}
