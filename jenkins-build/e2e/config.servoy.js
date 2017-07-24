5e1d780854028d982ed79d72c9ed87ac1eb051e9
alexzijlstra <azijlstra@servoy.com>
2017-07-24 15:38:48 +0200

    Changed features. The results of the test are also displayed in a log

--- jenkins-build/e2e/config.servoy.js
+++ jenkins-build/e2e/config.servoy.js
@@ -1,5 +1,4 @@
 var startDate; 
-var endDate; 
 exports.config = { 
   seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
   // seleniumAddress: 'selenium-hub:4444',
@@ -25,14 +24,18 @@ exports.config = {
 
   // Spec patterns are relative to this directory.
   specs: [
-    'features/sample_application/*.feature'
+    'features/sample_application/foundset.feature'
   ],
+
+  resultJsonOutputFile: 'reports/cucumber_reports/report.json',
 
   cucumberOpts: {
     require: ['features/step_definitions/servoy_step_definitions.js',
-              'env.js'],
+              'env.js'/*,
+              'features/step_definitions/hooks.js'*/],
     tags: false,
-    format: 'pretty',
+    // format: ['json:reports/cucumber_reports/report.json', 'pretty'],
+    format:'pretty',
     profile: false,
     keepAlive: false,
     'no-source': true
@@ -56,6 +59,7 @@ exports.config = {
       });
   },
 
+
   onComplete: () => {
       console.log('onComplete');
   },
@@ -65,8 +69,56 @@ exports.config = {
   },
 
   afterLaunch: () => {
-      console.log('afterLaunch');
-      endDate = new Date();      
-      console.log('Test(s) ended after: ' + ((endDate - startDate) / 1000) + ' seconds.');
+
+    console.log('afterLaunch');
+    var proc = require('process');
+    var fs = require('fs');
+    var json = fs.readFileSync(proc.cwd() + '/reports/cucumber_reports/report.json');
+    var a = JSON.parse(json);
+    var scenarioSucceeded = a.length;    
+    for(var x in a){
+      var duration = 0;
+      var failed = 0;
+      var succeeded = 0;
+      var skipped = 0;
+      var scenarioFailed = 0;
+
+      console.log("Scenarion: " + a[x].description);
+      for(var y in a[x].events){
+        console.log("\tSuccess/Fail: \t\t\t\t" + a[x].events[y].emitterEvent);
+        //bug in protractor, if it's skipped, the step still counts as passed
+        if(a[x].events[y].durationMillis > 0){
+          console.log("\tCucumber step: \t\t\t\t" + a[x].events[y].name);
+        } else {
+          console.log("\tCucumber step: \t\t\t\ttestSkipped");
+          skipped++;
+        }
+
+        console.log("\tDuration cucumber step: \t\t" + a[x].events[y].durationMillis + " miliseconds");
+        console.log();
+        if(a[x].events[y].durationMillis > 0){
+          duration+= a[x].events[y].durationMillis;
+        }
+        if(a[x].events[y].emitterEvent == "testPass" && a[x].events[y].durationMillis > 0){
+          succeeded++;
+        } else if(a[x].events[y].emitterEvent == "testFail") {
+          failed++;
+          scenarioSucceeded--;
+        } 
+      }
+      console.log('\tAmount of cucumber steps succeeded:\t' + succeeded);
+      console.log('\tAmount of cucumber steps failed:\t' + failed);
+      console.log('\tAmount of cucumber steps skipped:\t' + skipped);
+      console.log("\tTest ended after: " + duration + " miliseconds");
+      console.log();
+    }
+    console.log('Amount of scenarios succeeded:\t' + scenarioSucceeded);
+    console.log('Amount of scenarios failed:\t' + (a.length - scenarioSucceeded));
+    console.log('Test(s) ended after: ' + ((new Date() - startDate) / 1000) + ' seconds.');
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    // console.log(JSON.stringify(a, null, "    "));
   }
 };
--- jenkins-build/e2e/features/sample_application/hashing.feature
+++ jenkins-build/e2e/features/sample_application/hashing.feature
@@ -2,6 +2,7 @@ Feature: Testing the Servoy Sample Galery - Hashing
     Scenario Outline: Protractor and Cucumber Test
 
 Given I go to "http://demo.servoy.com/sampleGallery/solutions/sampleGallery/index.html?f=galleryMain"
+When servoy sidenav component with name "galleryMain.nav" tab "Working with Data" is clicked
 When servoy sidenav component with name "galleryMain.nav" tab "Secure Hashing" is clicked
 When servoy combobox component with name "exampleHash.algrothim" is clicked
 When servoy combobox component with name "exampleHash.algrothim" is clicked
@@ -9,7 +10,7 @@ When servoy combobox component with name "exampleHash.algrothim" the text <hash>
 When I press "enter"
 When default textarea component with name "exampleHash.plainText" the text "secret code" is inserted
 When servoy button component with name "exampleHash.svy_" is clicked
-Then I am done
+# Then I am done
 
 @data_table_servoy
 Examples:
