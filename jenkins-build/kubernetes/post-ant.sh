f50df2a8286e623907609020849fd5d0b75781ee
Victor Laza <vlaza@zeding.ro>
2017-07-25 15:34:37 +0300

    Commented out Tomcat curl deploy

--- jenkins-build/kubernetes/post-ant.sh
+++ jenkins-build/kubernetes/post-ant.sh
@@ -4,11 +4,11 @@
 #
 #
 
-echo "Undeploy applicaiton if exists.."
-curl "http://$T_USER:$T_PASS@tomcat-service:8080/manager/text/undeploy?path=/sampleGallery" || true
+#echo "Undeploy applicaiton if exists.."
+#curl "http://$T_USER:$T_PASS@tomcat-service:8080/manager/text/undeploy?path=/sampleGallery" || true
 
-echo "Uploading artifact into Tomcat.."
-curl --upload-file /var/lib/jenkins/workspace/svyMicroSamples/jenkins-build/build/export/sampleGallery.war "http://$T_USER:$T_PASS@tomcat-service:8080/manager/text/deploy?path=/sampleGallery"
+#echo "Uploading artifact into Tomcat.."
+#curl --upload-file /var/lib/jenkins/workspace/svyMicroSamples/jenkins-build/build/export/sampleGallery.war "http://$T_USER:$T_PASS@tomcat-service:8080/manager/text/deploy?path=/sampleGallery"
 
 #echo "Umounting NFS share from dev-app pod"
 #sudo umount /usr/local/servoy
